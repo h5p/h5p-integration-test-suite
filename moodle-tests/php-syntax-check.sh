bb3d5d382748a009ac256d26bbe858827d0d7dbb
thomasmars <thomasmars87@gmail.com>
2017-11-23 10:26:03 +0100

    Adds syntax test for Moodle for latest php fpm version.

--- moodle-tests/php-syntax-check.sh
+++ moodle-tests/php-syntax-check.sh
@@ -6,4 +6,10 @@ docker-compose --file docker-compose-old-php.yml exec oldphp bash -c \
 success=$?
 docker-compose --file docker-compose-old-php.yml down
 
+docker-compose --file docker-compose-latest-php.yml up -d
+docker-compose --file docker-compose-latest-php.yml exec latestphp bash -c \
+  "cd /var/www/html && find -type f -name "*.php" -print0 | xargs -0 -n 1 php -l"
+success=$?
+docker-compose --file docker-compose-latest-php.yml down
+
 exit "$success"
